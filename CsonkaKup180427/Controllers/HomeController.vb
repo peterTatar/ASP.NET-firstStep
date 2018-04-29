249211011ddb72429210903067187f30c47a5b1f
Peter Tatar <mail.peter.tatar@gmail.com>
2018-04-29 20:56:18 +0200

    My first change.

--- CsonkaKup180427/Controllers/HomeController.vb
+++ CsonkaKup180427/Controllers/HomeController.vb
@@ -6,7 +6,7 @@
     End Function
 
     Function About() As ActionResult
-        ViewData("Message") = "Your application description page."
+        ViewData("Message") = "Your application description page another change in my visual studio."
 
         Return View()
     End Function
